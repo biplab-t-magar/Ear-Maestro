45f59a14ad539401aeec76e516223a457d9227ab
Biplab Thapa Magar <biplab.t.magar@gmail.com>
2021-06-16 15:47:00 -0400

    added client side skeleton

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,8 @@ app.get('/api', (req, res) => {
     res.send('Hello world');
 });
 
-app.use(express.static('client-app/build'));
+app.use(express.static('client-side/build'));
 app.get('*', (req, res) => {
-    // res.send(path.resolve(__dirname, '..', 'client-app', 'build', 'index.html'));
     res.sendFile(path.resolve(__dirname, '..', 'client-app', 'build', 'index.html'));
 });
 
